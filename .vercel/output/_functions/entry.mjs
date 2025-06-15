5bc86e7bfd3a8ba5d85e1ddc1c13cef4b9eb02d1
germanhytsystem <germanhuaytalla22@gmail.com>
2025-06-15 13:59:51 -0500

    feat: update Vercel function manifest and middlewareSecret for improved functionality

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_BNSJ9xE3.mjs';
-import { manifest } from './manifest_-XIcuc2o.mjs';
+import { manifest } from './manifest_Bzg613K-.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "aadabe53-5b7a-4b33-b094-f64c4c6feec8",
+    "middlewareSecret": "63b1811d-8e9d-410b-ac84-65c9e6cc6cca",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
