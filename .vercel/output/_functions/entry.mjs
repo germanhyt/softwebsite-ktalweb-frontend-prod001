c8fed1e9cdab9bf1aa38bd1791fcae06ba01ac9e
germanhytsystem <germanhuaytalla22@gmail.com>
2025-06-15 13:56:25 -0500

    fix: update manifest import and middlewareSecret value for consistency

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_BNSJ9xE3.mjs';
-import { manifest } from './manifest_av6GIams.mjs';
+import { manifest } from './manifest_-XIcuc2o.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "080cddf8-2c4a-4b9f-a1ec-127442820b09",
+    "middlewareSecret": "aadabe53-5b7a-4b33-b094-f64c4c6feec8",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
