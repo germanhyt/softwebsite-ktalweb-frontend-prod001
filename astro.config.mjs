62f33f1efbaf506709c9669f7d198804586180eb
germanhytsystem <germanhuaytalla22@gmail.com>
2025-06-27 10:47:13 -0500

    feat: add sitemap integration and robots.txt for improved SEO and site indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,28 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  base: "/",
+  site: "https://ktalweb.com.pe",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      // filter: (page) => !page.includes("/admin/"), // Excluir
+      // customPages: [
+      //   "https://innovabcp2025.com/",
+      //   "https://innovabcp2025.com/preguntas-frecuentes",
+      // ],
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date("2025-06-27"),
+    }),
+  ],
 });
