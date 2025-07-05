f84cf16b81ec181997d56703d1482619379b2e1a
germanhytsystem <germanhuaytalla22@gmail.com>
2025-07-05 12:18:14 -0500

    feat: update components and styles for enhanced user experience and visual consistency across the portfolio

--- src/components/SolicitarCTABannerSection.tsx
+++ src/components/SolicitarCTABannerSection.tsx
@@ -11,22 +11,22 @@ const SolicitarCTABannerSection = () => {
     return (
         <>
             <section
-                className=""
-                style={{
-                    background: 'linear-gradient(90deg, #18003E 40%, #5919C1 90%)',
-                    color: '#FFFFFF',
-                }}
-            >
-                <div className='py-12 lg:py-16 container mx-auto flex flex-col md:flex-row items-center justify-between gap-8 rounded-lg'>
-                    <div className="max-w-lg 3xl:max-w-2xl text-start flex-1">
-                        <h2 className="font-nunito text-3xl md:text-4xl font-bold mb-4">
+                className="container mx-auto">
+                <div className='lg:pl-10 lg:py-0 mx-auto flex flex-col lg:flex-row items-center justify-between gap-8 rounded-xl overflow-hidden'
+                    style={{
+                        background: 'linear-gradient(90deg, #18003E 40%, #5919C1 90%)',
+                        color: '#FFFFFF',
+                    }}
+                >
+                    <div className="py-12 lg:pb-10 lg:w-[40%] max-w-lg 3xl:max-w-2xl text-start">
+                        <h2 className="font-nunito text-2xl md:text-3xl xl:text-4xl font-bold mb-2">
                             ¿No estás satisfecho con los resultados de tu web actual?
                         </h2>
                         <p className="font-nunito text-base mb-6">
                             Pide una auditoría gratuita hoy y te ayudaremos a potenciarla.
                         </p>
                         <button
-                            className="flex items-center gap-2 bg-primary-purple-200 text-white font-nunito font-semibold px-5 py-2 rounded-full transition bg-primary-purple-100 hover:cursor-pointer"
+                            className="flex items-center gap-2 bg-primary-purple-200 text-white font-nunito font-semibold px-5 py-2 rounded-full transition bg-primary-purple-100 hover:cursor-pointer hover:scale-105"
                             onClick={() => {
                                 if (isModalOpen) {
                                     setModalOpen(false);
@@ -40,18 +40,18 @@ const SolicitarCTABannerSection = () => {
                             <VscArrowCircleRight className="text-2xl" />
                         </button>
                     </div>
-                    <div className="flex-1 flex justify-center relative">
+                    <div className="lg:w-[60%]">
                         <img
                             src={k}
                             alt="Logo K"
-                            className="rounded-lg object-cover w-full max-w-md h-64 md:h-72"
+                            className=" object-cover w-full h-full"
                         />
                     </div>
                 </div>
             </section>
 
 
-            <ModalForm  isOpen={isModalOpen} onClose={() => setModalOpen(false)}  />
+            <ModalForm isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
 
         </>
     )
