0de9f99e801b50fb935fac39e943b4f04fdfaba9
germanhytsystem <germanhuaytalla22@gmail.com>
2025-07-04 21:33:42 -0500

    feat: add ClientsList and VideoSection components to enhance client showcase and visual content on the homepage

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -16,10 +16,16 @@ const HeroBanner = () => {
 
 
     return (
-        <div className="relative w-full  h-[40rem] md:h-[47rem] lg:h-[40rem] xl:h-[40rem] 3xl:h-[48rem] overflow-hidden">
+        <div className="
+            container mx-auto my-5 
+            relative 
+            w-full h-[40rem] md:h-[47rem] lg:h-[40rem] xl:h-[40rem] 3xl:h-[48rem]
+            overflow-hidden
+            rounded-2xl
+            ">
             {/* Video de fondo */}
             <video
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                className="absolute top-0 left-0 w-full h-full object-cover lg:translate-x-[125px] scale-100 lg:scale-125"
                 src={"https://res.cloudinary.com/dz0ajaf3i/video/upload/v1746928699/KTALWEB/hero_banner_ff0ijr.mp4"} // Reemplaza con la ruta de tu video
                 autoPlay
                 loop
@@ -33,7 +39,7 @@ const HeroBanner = () => {
                     <br className='flex' />
                     <div className='relative inline-block mt-3'>
 
-                        
+
                         <div
                             className="z-0 absolute inset-0 bg-gradient-to-r from-[#5919C1] to-[#3B206E] rounded-xl"
                             style={{ filter: "blur(16px)" }}
@@ -44,7 +50,7 @@ const HeroBanner = () => {
                     </div>
                 </h1>
                 <p className="font-nunito text-lg sm:text-2xl mt-6 max-w-xl">
-                    Creamos páginas webs que potencian tu presencia digital y reflejan lo que te hace único.
+                    Creamos soluciones que reflejan lo que te hace único.
                 </p>
                 <a
                     href="https://api.whatsapp.com/send?phone=51923416407&text=Hola%20ktalweb👋.%20Vi%20tu%20página%20web%20y%20me%20gustaría%20saber%20más%20sobre%20sus%20servicios.%20¿Me%20podrían%20ayudar,%C2%A0por%C2%A0favor?"
@@ -59,7 +65,8 @@ const HeroBanner = () => {
             </div>
 
             {/* Overlay para oscurecer el video */}
-            <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,.5)] backdrop-blur-sm"></div>
+            {/* <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,.4)] backdrop-blur-[2px]"></div> */}
+            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#000] to-[#000] opacity-60"></div>
         </div>
     );
 };
