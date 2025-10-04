c23fa1a89cab6ecbae6247afe6dff95c470788e3
germanhytsystem <germanhuaytalla22@gmail.com>
2025-10-03 20:51:34 -0500

    style: uncomment section in DescargarBrochureSection and update section ID in index.astro

--- src/components/DescargarBrochureSection.tsx
+++ src/components/DescargarBrochureSection.tsx
@@ -6,7 +6,7 @@ import { MdOutlineFileDownload } from "react-icons/md";
 const DescargarBrochureSection = () => {
     return (
         <>
-            {/* <section className="py-[6rem]  container mx-auto bg-white">
+            <section className="py-[6rem]  container mx-auto bg-white">
                 <h2
                     className="text-center font-nunito text-[1.5rem] md:text-3xl font-medium text-black mb-8"
                 >
@@ -19,7 +19,7 @@ const DescargarBrochureSection = () => {
                 <div className="flex justify-center">
                     <a
                         href="/brochure_ktalweb.pdf"
-                        download="/brochure_ktalweb.pdf"
+                        download="brochure_ktalweb.pdf"
                         type="application/pdf"
                         className="flex items-center gap-2 border-2 border-primary-purple-100 text-primary-purple-100 font-nunito font-semibold px-5 py-2 rounded-full transition hover:bg-primary-purple-100 hover:text-white"
                     >
@@ -27,7 +27,7 @@ const DescargarBrochureSection = () => {
                         <MdOutlineFileDownload className="text-2xl" />
                     </a>
                 </div>
-            </section> */}
+            </section>
         </>
     )
 }
